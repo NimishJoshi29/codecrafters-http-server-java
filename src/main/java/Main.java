1d888922be42051ce7c914343ff5628153a5d165
Nimish Joshi <joshinim29@gmail.com>
2024-05-11 19:51:29 +0530

    code restructure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,6 @@ public class Main {
     //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    String _200OKresponseString = "HTTP/1.1 200 OK\r\n\r\n";
-    String _404NOTFOUNDresponseString = "HTTP/1.1 404 Not Found\r\n\r\n";
 
     try {
       serverSocket = new ServerSocket(4221);
@@ -28,37 +26,40 @@ public class Main {
       OutputStream clientOutputStream = clientSocket.getOutputStream();
       InputStream clientInputStream = clientSocket.getInputStream();
 
-      BufferedReader reader = new BufferedReader(new InputStreamReader(clientInputStream));
+      respond(clientOutputStream, clientInputStream);
 
-      String clientRequestString = reader.readLine();
-      String requestPath = clientRequestString.split(" ")[1];
-
-      System.out.println(requestPath);
-
-      if (requestPath.length() == 1 && requestPath.equals("/")) {
-        System.out.println("Responded 200 OK");
-        clientOutputStream.write(_200OKresponseString.getBytes());
-      } else if (requestPath.length() > 1) {
-        String[] requestTokens = requestPath.split("/");
-
-        if (requestTokens[1].equals("echo")) {
-          String echoResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" + //
-              "Content-Length: " + requestTokens[2].length() + "\r\n" + //
-              "\r\n" + //
-              requestTokens[2];
-          clientOutputStream.write(echoResponse.getBytes());
-        } else {
-          System.out.println("Responded 404 NOT FOUND");
-          clientOutputStream.write(_404NOTFOUNDresponseString.getBytes());
-        }
-      }
-      clientOutputStream.flush();
-      serverSocket.close();
-      clientSocket.close();
     } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-      return;
+      e.printStackTrace();
     }
+  }
 
+  private static void respond(OutputStream clientOutputStream, InputStream clientInputStream) throws IOException {
+    String _200OKresponseString = "HTTP/1.1 200 OK\r\n\r\n";
+    String _404NOTFOUNDresponseString = "HTTP/1.1 404 Not Found\r\n\r\n";
+    BufferedReader reader = new BufferedReader(new InputStreamReader(clientInputStream));
+
+    String clientRequestString = reader.readLine();
+    String requestPath = clientRequestString.split(" ")[1];
+
+    System.out.println(requestPath);
+
+    if (requestPath.length() == 1 && requestPath.equals("/")) {
+      System.out.println("Responded 200 OK");
+      clientOutputStream.write(_200OKresponseString.getBytes());
+    } else if (requestPath.length() > 1) {
+      String[] requestTokens = requestPath.split("/");
+
+      if (requestTokens[1].equals("echo")) {
+        String echoResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" + //
+            "Content-Length: " + requestTokens[2].length() + "\r\n" + //
+            "\r\n" + //
+            requestTokens[2];
+        clientOutputStream.write(echoResponse.getBytes());
+      } else {
+        System.out.println("Responded 404 NOT FOUND");
+        clientOutputStream.write(_404NOTFOUNDresponseString.getBytes());
+      }
+    }
+    clientOutputStream.flush();
   }
 }
