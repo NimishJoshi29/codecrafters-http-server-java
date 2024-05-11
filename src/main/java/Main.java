4dd0096347424bb75e53165dd8c9d57dda6c6b87
Nimish Joshi <joshinim29@gmail.com>
2024-05-11 19:05:34 +0530

    respond with 404

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,15 +14,32 @@ public class Main {
     //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    String responseString = "HTTP/1.1 200 OK\r\n\r\n";
+    String _200OKresponseString = "HTTP/1.1 200 OK\r\n\r\n";
+    String _404NOTFOUNDresponseString = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
-      clientSocket.getOutputStream().write(responseString.getBytes());
+      OutputStream clientOutputStream = clientSocket.getOutputStream();
+      InputStream clientInputStream = clientSocket.getInputStream();
+
+      String clientRequestString = new String(clientInputStream.readAllBytes());
+      System.out.println(clientRequestString);
+      String requestPath = clientRequestString.split("\r\n\r\n")[0].split(" ")[1];
+
+      System.out.println(requestPath);
+
+      if (requestPath.equals("/")) {
+        System.out.println("Responded 200 OK");
+        clientOutputStream.write(_200OKresponseString.getBytes());
+      } else {
+        System.out.println("Responded 404 NOT FOUND");
+        clientOutputStream.write(_404NOTFOUNDresponseString.getBytes());
+      }
       serverSocket.close();
+      clientSocket.close();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
       return;
