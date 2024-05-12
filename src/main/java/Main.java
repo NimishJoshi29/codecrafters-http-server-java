8c2a6afe202c4d0328d92400bf159f2d97da45e5
Nimish Joshi <joshinim29@gmail.com>
2024-05-12 11:55:27 +0530

    read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
 
 public class Main {
   public static void main(String[] args) {
@@ -36,6 +39,7 @@ public class Main {
   private static void respond(OutputStream clientOutputStream, InputStream clientInputStream) throws IOException {
     String _200OKresponseString = "HTTP/1.1 200 OK\r\n\r\n";
     String _404NOTFOUNDresponseString = "HTTP/1.1 404 Not Found\r\n\r\n";
+
     BufferedReader reader = new BufferedReader(new InputStreamReader(clientInputStream));
 
     String clientRequestString = reader.readLine();
@@ -55,6 +59,28 @@ public class Main {
             "\r\n" + //
             requestTokens[2];
         clientOutputStream.write(echoResponse.getBytes());
+      } else if (requestTokens[1].equals("user-agent")) {
+
+        List<Byte> inputBytesList = new ArrayList<Byte>();
+        while (reader.ready())
+          inputBytesList.add((byte) reader.read());
+
+        byte[] inputBytesArray = new byte[inputBytesList.size()];
+        int i = 0;
+        for (byte b : inputBytesList)
+          inputBytesArray[i++] = b;
+
+        String fullRequestString = new String(inputBytesArray);
+
+        String userAgent = fullRequestString.substring(fullRequestString.indexOf("User-Agent")).split("\r")[0]
+            .split(":")[1].stripLeading();
+
+        String userAgentEchoResponseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+            + userAgent.length() + "\r\n\r\n" + userAgent;
+
+        clientOutputStream.write(userAgentEchoResponseString.getBytes());
+
+        System.out.println("Responded 200 OK with user-agent info");
       } else {
         System.out.println("Responded 404 NOT FOUND");
         clientOutputStream.write(_404NOTFOUNDresponseString.getBytes());
